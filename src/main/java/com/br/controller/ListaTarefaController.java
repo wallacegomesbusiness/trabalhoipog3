94e65b9cb1e69a34251b7d00d842f7a97b45c96b
Wallace Gomes <wallacemesqti@gmail.com>
2025-11-23 20:40:08 -0300

    Atividade da Aula 3 do projeto de lista

--- src/main/java/com/br/controller/ListaTarefaController.java
+++ src/main/java/com/br/controller/ListaTarefaController.java
@@ -1,15 +1,21 @@
 package com.br.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.br.exception.ResourceNotFoundException;
 import com.br.model.ListaTarefa;
 import com.br.repository.ListaTarefaRepository;
 
@@ -23,13 +29,61 @@ public class ListaTarefaController {
 	@Autowired
 	private ListaTarefaRepository lRep;
 	
+	// Lista todos os usúarios:
+	// GET - http://localhost:8080/listatarefa/lista
 	@GetMapping("/lista")
+	public List<ListaTarefa> listar(){
+		return this.lRep.findAll();
+	}
 	
+	// Consultar um usuario específico:
+	// GET - http://localhost:8080/listatarefa/lista/{id}
+	@GetMapping("/lista/{id}")
+	public ResponseEntity<ListaTarefa> consultar(@PathVariable Long id){
+		ListaTarefa lista = this.lRep.findById(id).orElseThrow(() -> 
+		new ResourceNotFoundException("Usuário não encontrado: " + id));
+		
+		return ResponseEntity.ok(lista);
+	};
+	
+	// Inserir um novo usuário
+	// POST - http://localhost:8080/listatarefa/lista
 	@PostMapping("/lista")
+	public ListaTarefa inserir(@RequestBody ListaTarefa lista) {
+		return this.lRep.save(lista);
+	}
 	
+	// Alterar um  usuário existente
+	// PUT - http://localhost:8080/listatarefa/lista/{id}
 	@PutMapping("/lista/{id}")
+	public ResponseEntity<ListaTarefa> alterar(@PathVariable Long id,@RequestBody ListaTarefa lista){
+		ListaTarefa list = this.lRep.findById(id).orElseThrow(() ->
+		new ResourceNotFoundException("Usuário não encontrado: " + id));
+		
+		list.setNome(lista.getNome());
+		list.setStatus(lista.getStatus());
+		list.setTarefa(lista.getTarefa());
+		
+		ListaTarefa listaAtualizado = lRep.save(list);
+		
+		return ResponseEntity.ok(listaAtualizado);
+	}
 	
+	// Excluir um usuário existente
+	// DELETE - http://localhost:8080/listatarefa/lista/{id}
 	@DeleteMapping("/lista/{id}")
+	public ResponseEntity<Map<String, Boolean>> excluir(@PathVariable Long id){
+		
+		ListaTarefa lista = this.lRep.findById(id).orElseThrow(() ->
+		new ResourceNotFoundException("usuário não encontrado: " + id));
+		
+		this.lRep.delete(lista);
+		
+		Map<String, Boolean> resposta = new HashMap<>();
+		resposta.put("Usuário excluído", Boolean.TRUE);
+		
+		return ResponseEntity.ok(resposta);
+	} 
 	
 	
 	
